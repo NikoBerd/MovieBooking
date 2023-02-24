7c6e1dca3cd37b5775cf643c5d127afebd589219
Niko Berdzenishvili <nikolozisia@gmail.com>
2023-02-24 22:58:43 +0400

    Initial Commit

---
+++ MovieBooking/Data/TabData.swift
@@ -0,0 +1,16 @@
+//
+//  TabData.swift
+//  MovieBooking
+//
+//  Created by Niko on 24.02.23.
+//
+
+import Foundation
+
+enum Tab: String, CaseIterable {
+  case home     = "Home"
+  case location = "Location"
+  case ticket   = "Ticket"
+  case category = "Category"
+  case profile  = "Profile"
+}
